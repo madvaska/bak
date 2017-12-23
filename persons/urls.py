b03a93de31250307e2d5258308c8dcb8eb15db86
madvaska <netak.su@gmail.com>
2017-12-23 22:29:03 +0700

    2017-12-23-5

--- persons/urls.py
+++ persons/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
+#from django.contrib import admin
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^persons/', include('persons.urls')),
+    url(r'^$', views.persons, name='сотрудники'),
 ]
--- persons/views.py
+++ persons/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Person
 # Create your views here.
+
+def persons(request):
+    return render(request, 'persons/persons.html', {})
