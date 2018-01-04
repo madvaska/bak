951918b02c07025fa1b9a4b8c57261d198a01c33
madvaska <netak.su@gmail.com>
2018-01-04 23:29:13 +0700

    2018-01-04

--- persons/views.py
+++ persons/views.py
@@ -6,3 +6,10 @@ def persons(request):
     emps = Person.objects.all()
     #print(emps)
     return render(request, 'persons/persons.html', {'emps':emps})
+
+
+class PersonCreateView(CreateView):
+    model = Person
+    form_class = FORM_CLASS
+    success_url = 'SUCCESS_URL'
+    template_name = 'TEMPLATE_NAME'
