7b616480307bfc2079378c841ffcf291bafbb70b
MadVaska <netak.su@gmail.com>
2018-01-22 17:48:47 +0700

    2018-01-22 3

--- analyzes/urls.py
+++ analyzes/urls.py
@@ -17,12 +17,21 @@ from django.conf.urls import url
 from . import views
 from persons.views import AddPersons
 from django.views.generic import CreateView
-from analyzes.views import AddOrder
 #from django.contrib import admin
+from analyzes.views import AddOrder
+from analyzes.views import AddAnalyzeType
+from analyzes.views import AddProject
+from analyzes.views import AddAnalyze
 
 
 urlpatterns = [
     #url(r'^$', views.persons, name='сотрудники'),
     url(r'^o/$', views.orders, name='сотрудники'),
     url(r'^o/add', AddOrder.as_view(), name='сотрудники'),
+    url(r'^at/$', views.analyzeType, name='Типы анализов'),
+    url(r'^at/add', AddAnalyzeType.as_view(), name='Добавить новый тип анализов'),
+    url(r'^pr/$', views.projects, name='Проекты'),
+    url(r'^pr/add', AddProject.as_view(), name='Добавить новый проект'),
+    url(r'^a/$', views.analyzes, name='Анализы'),
+    url(r'^a/add', AddAnalyze.as_view(), name='Добавить новый анализ'),
 ]
--- analyzes/views.py
+++ analyzes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Order, Project, AnalyzeType
+from .models import Order, Project, AnalyzeType, Analyze
 from persons.models import Customer
 from django.views.generic.edit import CreateView
 
@@ -16,3 +16,43 @@ class AddOrder(CreateView):
     fields = ['dateTime','code','codeOfSample','type','customer','project','comment','executed']
     #print('emps')
     #return render(request, 'persons/persons.html', {'emps':emps})
+
+def analyzeType(request):
+    analyzeTypes = AnalyzeType.objects.all()
+    #print(emps)
+    return render(request, 'analyzes/analyzetypes.html', {'analyzeTypes':analyzeTypes})
+
+class AddAnalyzeType(CreateView):
+    model = AnalyzeType
+    fields = ['name']
+    #print('emps')
+    #return render(request, 'persons/persons.html', {'emps':emps})
+
+def projects(request):
+    projects = Project.objects.all()
+    #print(emps)
+    return render(request, 'analyzes/projects.html', {'projects':projects})
+
+class AddProject(CreateView):
+    model = Project
+    fields = ['name']
+    #print('emps')
+    #return render(request, 'persons/persons.html', {'emps':emps})
+
+def analyzes(request):
+    analyzes = Analyze.objects.all()
+    #print(emps)
+    return render(request, 'analyzes/projects.html', {'analyzes':analyzes})
+
+class AddAnalyze(CreateView):
+    model = Analyze
+    fields = ['dateTime']
+    # Создается на основании заявки
+    #print('emps')
+    #return render(request, 'persons/persons.html', {'emps':emps})
+
+
+#    url(r'^at/$', views.analyzeType, name='Типы анализов'),
+#    url(r'^at/add', AddAnalyzeType.as_view(), name='Добавить новый тип анализов'),
+#    url(r'^pr/$', views.projects, name='Проекты'),
+#    url(r'^pr/add', AddProject.as_view(), name='Добавить новый проект'),
